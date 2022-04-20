0541d45350f15d34d8178315481837683a0d1343
annaweistn <101801514+annaweistn@users.noreply.github.com>
2022-04-20 19:33:51 +0200

    cardscontainer

---
+++ Kate/Vorlesung_5/loops.js
@@ -0,0 +1,18 @@
+// const arr = [1,2,3,4,5,6,7,8]
+// function evenNum(arr){
+    // for(let i=0; i<arr.length; i++){
+       //  if (arr[i] % 2 === 0){
+          //  console.log(arr[i])
+        // }
+    // }
+// }
+// evenNum(arr)
+
+// Task 3
+const arr = [1,2,3,5,6]
+function getReverseNumbers(arr){
+    for(let i = arr.lenght; i >=0; i-- ){
+        console.log(arr[i])
+    }
+}
+getReverseNumbers(arr)
---
+++ Kate/Vorlesung_5_1/index.js
@@ -0,0 +1,23 @@
+// parent element - div id = cards-container
+// only for ids
+const cardsContainer = document.getElementById('cards-container')
+// e.g. for class
+document.querySelector('your-class')
+
+const createCards = () =>{
+    for (let i=1; i < 6; i++) {
+        //create elemnt
+        const card = document.createElement('div')
+        //add some content
+        card.innerHTML = `<p>Element number ${i} </p>`
+        // add styles - class
+        card.classList.add('card')
+        //apend element
+        cardsContainer.appendChild(card)
+    }
+
+
+}
+createCards()
+{
+   }
