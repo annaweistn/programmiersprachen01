b2db4fb2c6b365e063a0ab0e1e57fd9a10c0594f
annaweistn <101801514+annaweistn@users.noreply.github.com>
2022-04-27 19:41:18 +0200

    Lesson_6

---
+++ Asisa/Vorlesung_06/sketch.js
@@ -0,0 +1,14 @@
+let x = "6";
+let y = "7";
+function average(x, y) {
+  return (x + y) / 2;
+}
+result = average (x,y);
+console.log(result);
+
+let stringX = "6";
+let stringY = "7";
+
+
+
+
--- Kate/Vorlesung_5_1/index.js
+++ Kate/Vorlesung_5_1/index.js
@@ -1,23 +1,53 @@
-// parent element - div id = cards-container
+//parent element - div id = cards-container
 // only for ids
 const cardsContainer = document.getElementById('cards-container')
 // e.g. for class
-document.querySelector('your-class')
+// document.querySelector('your-class')
 
-const createCards = () =>{
-    for (let i=1; i < 6; i++) {
-        //create elemnt
+const addButton = document.getElementById('add-btn')
+const removeButton = document.getElementById('remove-btn')
+const toggleBth = document.getElementById('toggle-btn')
+
+const createCards = () => {
+    removeCards()
+    for (let i = 1; i < 6; i++) {
+        // create element 
         const card = document.createElement('div')
         //add some content
         card.innerHTML = `<p>Element number ${i} </p>`
         // add styles - class
         card.classList.add('card')
-        //apend element
+        //append element
         cardsContainer.appendChild(card)
     }
-
-
 }
-createCards()
-{
-   }
+
+const removeCards = () => {
+    // check if cardsContainer has child elements 
+    if (cardsContainer.hasChildNodes()) {
+        //remove child elements 
+        while (cardsContainer.firstChild) {
+            cardsContainer.removeChild(cardsContainer.firstChild)
+        }
+    }
+}
+
+const toggleClass = () => {
+    if (!cardsContainer.hasChildNodes()) {
+        return
+    }
+
+    const cards = document.querySelectorAll('.card')
+
+    console.log(cards)
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].classList.toggle('card--dark')
+    }
+}
+
+
+// addEventListener - attaches an event handler to an element
+// element.addEventListener(event, function)
+addButton.addEventListener('click', createCards)
+removeButton.addEventListener('click', removeCards)
+toggleBth.addEventListener('click', toggleClass)
