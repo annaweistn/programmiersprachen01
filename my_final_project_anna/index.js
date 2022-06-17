4e05c49ab34c24417005119c993a478fead68581
annaweistn <101801514+annaweistn@users.noreply.github.com>
2022-06-17 16:25:05 +0200

    improvement

--- my_final_project_anna/index.js
+++ my_final_project_anna/index.js
@@ -1,41 +1,41 @@
-var formular = document.querySelector("form");
-var endeContainer = document.getElementById("endeSeite");
-var startContainer = document.getElementById("startSeite");
-var spielContainer = document.getElementById("spielSeite");
+window.addEventListener("click", () => {
+  document.getElementById("song").play();
+});
+const formular = document.querySelector("form");
+const endeContainer = document.getElementById("endeSeite");
+const startContainer = document.getElementById("startSeite");
+const spielContainer = document.getElementById("spielSeite");
 spielContainer.style = "display: none;";
 endeContainer.style = "display: none;";
 
-var navScore = document.getElementById("navbar");
+const navScore = document.getElementById("navbar");
 navScore.style = "display: none;";
-var computer = document.getElementById("computer");
-var user = document.getElementById("user");
+const computer = document.getElementById("computer");
+const user = document.getElementById("user");
 
-var spielerAuswahl = document.getElementById("spielerAuswahl");
-var computerAuswahl = document.getElementById("computerAuswahl");
+let spielerAuswahl = document.getElementById("spielerAuswahl");
+let computerAuswahl = document.getElementById("computerAuswahl");
 
-var punkt = document.getElementById("point");
+const punkt = document.getElementById("point");
 
-var laden = document.getElementById("laden");
+const laden = document.getElementById("laden");
 laden.style = "display:none;";
 
-var username;
+let username;
 
-var score = {
+let score = {
   user: 0,
   computer: 0,
 };
-var auswahlComputer = ["scissors", "stone", "paper"];
+const auswahlComputer = ["scissors", "stone", "paper"];
 
 formular.addEventListener("submit", function (event) {
   event.preventDefault();
   username = event.target[0].value;
   navScore.style = "display: flex;";
-  console.log(event.target[0].value);
   spielContainer.style = "display: block";
   startContainer.style = "display: none";
-  //var benutzerAnzeige = document.getElementById('user');//wird nie genutzt
-  console.log("hello!");
-  user.innerHTML = username + "0/3";
+  user.innerHTML = username + " 0/3";
 });
 
 function play(event) {
@@ -45,24 +45,26 @@ function play(event) {
   computerAuswahl.style = "display: none";
   punkt.style = "display:none;";
   laden.style = "display:flex;justify-content: center;width:100%;";
-  setTimeout(function () {
-    /*time loading */ laden.style = "display: none;";
-    if (spielerWert == "scissors" && computerWert == "paper") {
+
+    setTimeout(function () {
+ /*time loading */ laden.style = "display: none;";
+    
+     if (spielerWert === "scissors" && computerWert === "paper") {
       score.user += 1;
       punkt.style = "display:flex;justify-content: center;width:100%;";
-    } else if (spielerWert == "scissors" && computerWert == "stone") {
+    } else if (spielerWert === "scissors" && computerWert === "stone") {
       score.computer += 1;
     }
-    if (spielerWert == "stone" && computerWert == "paper") {
+    if (spielerWert === "stone" && computerWert === "paper") {
       score.computer += 1;
-    } else if (spielerWert == "stone" && computerWert == "scissors") {
+    } else if (spielerWert === "stone" && computerWert === "scissors") {
       score.user += 1;
       punkt.style = "display:flex;justify-content: center;width:100%;";
     }
 
-    if (spielerWert == "paper" && computerWert == "scissors") {
+    if (spielerWert === "paper" && computerWert === "scissors") {
       score.computer += 1;
-    } else if (spielerWert == "paper" && computerWert == "stone") {
+    } else if (spielerWert === "paper" && computerWert === "stone") {
       score.user += 1;
       punkt.style = "display:flex;justify-content: center;width:100%;";
     }
@@ -79,8 +81,8 @@ function play(event) {
     spielerAuswahl.children[1].innerHTML = "Your choice: " + spielerWert;
     computerAuswahl.children[1].innerHTML = "Computer choice: " + computerWert;
 
-    if (score.user == 3 || score.computer == 3) {
-      if (score.user == 3) {
+    if (score.user === 3 || score.computer === 3) {
+      if (score.user === 3) {
         finish("user");
       } else {
         finish("computer");
@@ -92,7 +94,7 @@ function finish(winner) {
   spielerAuswahl.style = "display: none";
   computerAuswahl.style = "display: none";
   spielContainer.style = "display: none";
-  if (winner == "user") {
+  if (winner === "user") {
     endeContainer.style = "display: flex;";
     endeContainer.children[0].innerHTML =
       "<p>You won!</p><img src='./bilder/pokal.png'>";
@@ -106,7 +108,7 @@ function restart() {
   score.user = 0;
   spielContainer.style = "display: block";
   endeContainer.style = "display: none;";
-  computer.innerHTML = "Computer: " + score.computer + "/3";
+  computer.innerHTML = "Computer " + score.computer + "/3";
   user.innerHTML = username + " " + score.user + "/3";
   /* Konfetti emoji muss verschwinden wenn man gewonnen hat */
   punkt.style = "display:none;";
