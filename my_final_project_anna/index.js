15141a53c7bf3254e643c69820397a2506a27980
Ekaterina Fedotova <fedotova490@gmail.com>
2022-06-17 14:24:49 +0200

    Add review comments

--- my_final_project_anna/index.js
+++ my_final_project_anna/index.js
@@ -1,7 +1,11 @@
+// "var" was much in use before "let" and "const" appeared, now we don't need it anymore
+// use "const" any time you select an element or you have a variable that doesn't change
+// use "let" if value is going to change
 var formular = document.querySelector("form");
 var endeContainer = document.getElementById("endeSeite");
 var startContainer = document.getElementById("startSeite");
 var spielContainer = document.getElementById("spielSeite");
+// create a class "hidden" in your index.css, put it by default to the elements in your index.html and remove it when needed 
 spielContainer.style = "display: none;";
 endeContainer.style = "display: none;";
 
@@ -30,11 +34,14 @@ formular.addEventListener("submit", function (event) {
   event.preventDefault();
   username = event.target[0].value;
   navScore.style = "display: flex;";
+  // please remove any console.log statements, they are only needed for development :)
   console.log(event.target[0].value);
   spielContainer.style = "display: block";
   startContainer.style = "display: none";
+  // please remove any unused code
   //var benutzerAnzeige = document.getElementById('user');//wird nie genutzt
   console.log("hello!");
+  // add space: username + " 0/3"
   user.innerHTML = username + "0/3";
 });
 
@@ -44,9 +51,17 @@ function play(event) {
   spielerAuswahl.style = "display: none";
   computerAuswahl.style = "display: none";
   punkt.style = "display:none;";
+  // if you have several styles to assign, move them to a separate class in css and use "classList.add("name of your class")" or "classList.remove("name of your class")" "classList.toggle("name of your class")"
   laden.style = "display:flex;justify-content: center;width:100%;";
+
+  // while we are waiting for the timeout function to be executed, we can press another button many times and then the game can become buggy.
+  // to fix this you can disable the buttons whenever you pressed a button and enable them as soon as you get the result
+  // you can use "element.setAttribute("disabled", true)" to disable a button and "element.removeAttribute("disabled")" to enable them
+
   setTimeout(function () {
+    // move all the content of this function to a separate one, e.g. setResults
     /*time loading */ laden.style = "display: none;";
+    // always better use === instead of ==, remember that Javascript will return "true" to "1 == true" and it's not really true :) so you need this strict comparison
     if (spielerWert == "scissors" && computerWert == "paper") {
       score.user += 1;
       punkt.style = "display:flex;justify-content: center;width:100%;";
@@ -66,6 +81,9 @@ function play(event) {
       score.user += 1;
       punkt.style = "display:flex;justify-content: center;width:100%;";
     }
+
+    // a small suggestion how you can write the game logic in less lines: the 1st answer here :) https://stackoverflow.com/questions/53730900/more-efficient-choice-comparison-for-rock-paper-scissors 
+
     computer.innerHTML = "Computer " + score.computer + "/3";
     user.innerHTML = username + " " + score.user + "/3";
 
